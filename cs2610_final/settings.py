1beab95081be2c914ad3353b8a895022e16a3da0
mattofthekidd <mattofthekidd@gmail.com>
2017-04-26 22:40:09 +0000

    Final version, can't remove records w/o deleting the whole table at present. Hopefully I will find time to fix that before grading.

--- cs2610_final/settings.py
+++ cs2610_final/settings.py
@@ -32,6 +32,8 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Application definition
 
 INSTALLED_APPS = [
+    'final.apps.FinalConfig',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'MST'
 
 USE_I18N = True
 
